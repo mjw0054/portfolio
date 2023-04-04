ff4f81036720c28390e5a555fb97202abd956427
mjw0054 <mjw0054@gmail.com>
2023-04-04 18:30:19 +0900

    update

--- routes.js
+++ routes.js
@@ -1,51 +1,37 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
+  res.write("<html>");
+  res.write("<head>");
+  res.write('<meta charset="utf-8"/>');
+  res.write("<title>Kakao 지도 시작하기</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write('<div id="map" style="width:500px;height:400px;"></div>');
+  res.write(
+    '<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=121dd739ce760b5a8b6b13be460f16b2"></script>'
+  );
+  res.write("<script>");
+  res.write("var container = document.getElementById(`map`);");
+  res.write("var options = {");
+  res.write("center: new kakao.maps.LatLng(33.450701, 126.570667),");
+  res.write("level: 3");
+  res.write("};");
+
+  res.write("var map = new kakao.maps.Map(container, options);");
+  res.write("</script>");
+  res.write("</body>");
+  res.write("</html>");
+
+  /*res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
-    res.write('</html>');
-    return res.end();
-  }
-  
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
+    res.write('</html>');*/
+  return res.end();
 };
-
-// module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some text';
-
 exports.handler = requestHandler;
-exports.someText = 'Some hard coded text';
+exports.someText = "Some hard coded text";
